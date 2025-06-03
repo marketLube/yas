7c4f83ad93144e722e5572f56f38b798e4010164
marketlubes <marketlubes@gmail.com>
2025-06-03 19:38:34 +0530

    Refactor SideBar component to utilize state for active item management; streamline item rendering with a map function; enhance styles for sidebar items and hover effects; adjust layout and border styles for improved visual consistency.

--- src/layouts/SideBar/SideBar.jsx
+++ src/layouts/SideBar/SideBar.jsx
@@ -1,87 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../components/Logo/Logo";
 import ChatWithUsButton from "../../components/buttons/ChatWithUsButton";
-import dash from "../../assets/icons/dash.svg"
-import beach from "../../assets/icons/beach.svg"
-import dropbox from "../../assets/icons/dropbox.svg"
-import house from "../../assets/icons/house.svg"
-import chef from "../../assets/icons/chef.svg"
-import camera from "../../assets/icons/cam.svg"
-import shop from "../../assets/icons/shop.svg"
-
-
-
-
+import dash from "../../assets/icons/dash.svg";
+import beach from "../../assets/icons/beach.svg";
+import dropbox from "../../assets/icons/dropbox.svg";
+import house from "../../assets/icons/house.svg";
+import chef from "../../assets/icons/chef.svg";
+import camera from "../../assets/icons/cam.svg";
+import shop from "../../assets/icons/shop.svg";
 
 export default function SideBar() {
+  const [activeItem, setActiveItem] = useState("All");
+
+  const handleClick = (item) => {
+    setActiveItem(item);
+  };
+
+  // Define the sidebar items
+  const sideBarItems = [
+    { name: "All", icon: dash },
+    { name: "Top Attractions", icon: beach },
+    { name: "Packages", icon: dropbox },
+    { name: "Hotels", icon: house },
+    { name: "Dining", icon: chef },
+    { name: "Live", icon: camera },
+    { name: "Shopping", icon: shop },
+  ];
+
   return (
     <nav className="side-bar normal-title-txt">
       {/* <Logo /> */}
       <ul className="side-bar-list">
-        <NavLink to="/product">
-          <li className="side-bar-list-item">
-            <div className="item-icon">
-              {/* Add your icon img tag here */}
-              <img src={dash} alt="All" />
-            </div>
-            <div className="item-text">All</div>
-          </li>
-        </NavLink>
-        <NavLink to="/product">
-          <li className="side-bar-list-item">
-             <div className="item-icon">
-              {/* Add your icon img tag here */}
-              <img src={beach} alt="Top Attractions" />
-            </div>
-            <div className="item-text">Top Attractions</div>
-          </li>
-        </NavLink>
-        <NavLink to="/product">
-          <li className="side-bar-list-item">
-             <div className="item-icon">
-              {/* Add your icon img tag here */}
-              <img src={dropbox} alt="Packages" />
-            </div>
-            <div className="item-text">Packages</div>
-          </li>
-        </NavLink>
-        <NavLink to="/product">
-          <li className="side-bar-list-item">
-             <div className="item-icon">
-              {/* Add your icon img tag here */}
-              <img src={house} alt="Hotels" />
-            </div>
-            <div className="item-text">Hotels</div>
-          </li>
-        </NavLink>
-        <NavLink to="/product">
-          <li className="side-bar-list-item">
-             <div className="item-icon">
-              {/* Add your icon img tag here */}
-              <img src={chef} alt="Dining" />
-            </div>
-            <div className="item-text">Dining</div>
-          </li>
-        </NavLink>
-        <NavLink to="/product">
-          <li className="side-bar-list-item">
-             <div className="item-icon">
-              {/* Add your icon img tag here */}
-              <img src={camera} alt="Live" />
-            </div>
-            <div className="item-text">Live</div>
-          </li>
-        </NavLink>
-        {/* <NavLink to="/product">
-          <li className="side-bar-list-item">
-             <div className="item-icon">
-              
-              <img src={shop} alt="Shopping" />
-            </div>
-            <div className="item-text">Shopping</div>
-          </li>
-        </NavLink> */}
+        {sideBarItems.map((item) => (
+          <div
+            className={`side-bar-list-item-container ${
+              activeItem === item.name ? "active" : ""
+            }`}
+            onClick={() => handleClick(item.name)}
+          >
+            <li
+              key={item.name}
+              className="side-bar-list-item "
+              onClick={() => handleClick(item.name)}
+            >
+              <div className="item-icon">
+                <img src={item.icon} alt={item.name} />
+              </div>
+              <div className="item-text">
+                {item.name.split(" ").map((word, index) => (
+                  <div key={index}>{word}</div>
+                ))}
+              </div>
+            </li>
+          </div>
+        ))}
       </ul>
       <div className="side-bar-bottom">
         <ChatWithUsButton />
